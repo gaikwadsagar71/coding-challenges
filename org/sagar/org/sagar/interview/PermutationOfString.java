b90e74b23cb44e508a3dc47a52a0669ebfa389da
Sagar Gaikwad <gaikwadsagar71@gmail.com>
2023-01-14 12:01:49 +0530

    add code

--- org/sagar/org/sagar/interview/PermutationOfString.java
+++ org/sagar/org/sagar/interview/PermutationOfString.java
@@ -1,2 +1,35 @@
-package org.sagar.org.sagar.interview;public class PermutationOfString {
+package org.sagar.org.sagar.interview;
+
+public class PermutationOfString {
+
+    static int stringLen;
+
+    public static void main(String[] args) {
+        String input = "abcd";
+        String answer = " ";
+
+        stringLen = input.length();
+        System.out.println("Output is: ");
+        printPermution(input, answer);
+    }
+
+    private static void printPermution(String input, String answer) {
+
+        if (input.length() == 0) {
+            System.out.print(answer + " ");
+
+            return;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+
+            String firstPart = input.substring(0, i);
+            String lastPart = input.substring(i+1);
+
+            printPermution(firstPart + lastPart, answer + character);
+
+            if (input.length() == stringLen)  System.out.println("");
+        }
+    }
 }
